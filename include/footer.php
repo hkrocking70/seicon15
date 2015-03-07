bb8dcc61c320a5ec31e21d1fc3731af8b75beb69
garvit1608 <garvit1608@gmail.com>
2015-03-08 02:17:10 +0530

    header fixed

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,10 @@
-<div class="navbar navbar-fixed-bottom footer">
+<div class= "navbar navbar-fixed-bottom  footer" >
   <div class="container-fluid" style="padding-top: 10px;">
     <div class="row container-fluid">
 
 
 
-      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 container-fluid" style="margin-top:6px;">
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 " style="margin-top:6px;">
         <a href="https://www.facebook.com/pages/SEICON/894757557243320"><i class="fa fa-facebook fa-2x fb" style="color:#fff;"></i></a>
         &nbsp;&nbsp;
         <a href="#"><i class="fa fa-youtube-play fa-2x youtube" style="color:#fff;"></i></a>
@@ -15,14 +15,14 @@
       <div class="col-lg-2 col-md-2 col-sm-0 col-xs-0"></div>
 
       <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4" style="padding-top:8px;">
-      <span style="color:#FFF; font-size:12px; ">© Copyrights Webarch 2015</span>
+      <span style="color:#FFF; font-size:12px; ">&copy; Copyrights Webarch 2015</span>
       </div>
 
       <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1"></div>
 
       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" style="text-align:center">
         <span class="webarch">
-        <img src="images/webarch.png" alt="Webarch" width="48%" height="80%" >
+        <img src="images/webarch.png" alt="Webarch" width="48%"  >
         </span>
       </div>
 
--- include/header.php
+++ include/header.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="row">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -8,12 +9,21 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
+<!--
                 <a class="navbar-brand" href="index.php">
                     <img src="images/logo2.jpg" alt="" class="img-responsive" width="150px">
                 </a>
+-->
+                
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <div class="col-md-3" style="margin-top:10px;">
+                 <img src="images/logo2.jpg" alt="" class="img-responsive seicon-logo" width="200px"  >
+            </div>
+            <div class="col-md-7">
+            <div class="collapse navbar-collapse"  id="bs-example-navbar-collapse-1">
+                
                 <ul class="nav navbar-nav">
                     <li class="vision">
                         <a href="index.php">Our Vision</a>
@@ -31,10 +41,18 @@
                         <a href="contact.php">Contact Us</a>
                     </li>
                 </ul>
+
             </div>
+        </div>
+        
+        <div class="col-md-2 srm-logo" >
+            <img src="images/logo.gif" width="110" height="13%">
+        </div>
+
+    
             <!-- /.navbar-collapse -->
           
-
+   </div>
         </div>
         <!-- /.container -->
     </nav>
