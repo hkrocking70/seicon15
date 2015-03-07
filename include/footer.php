24605bc52653552cdca39010b32d2ae5df18a180
Himanshu <himanshu.kumar133@gmail.com>
2015-03-07 17:00:27 +0530

    Seicon 15 News update and icon update

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,11 @@
 <div class="navbar navbar-fixed-bottom footer">
   <div class="container" style="padding-top: 10px;">
-    <!--<a href="https://www.facebook.com/pages/SEICON/894757557243320"><img src="images/fb.png" height="4%"></a>-->
     <a href="https://www.facebook.com/pages/SEICON/894757557243320"><i class="fa fa-facebook fa-2x fb" style="color:#fff;"></i></a>
     &nbsp;&nbsp;
-    <!--<a href="#"><img src="images/youtube.png" height="4%"></a>-->
-    <A href="#"><i class="fa fa-youtube-play fa-2x youtube" style="color:#fff;"></i></a>
+    <a href="#"><i class="fa fa-youtube-play fa-2x youtube" style="color:#fff;"></i></a>
+    &nbsp;&nbsp;
+    <a href="https://play.google.com/store/apps/details?id=com.appenstien.seicon"><i class="fa fa-android fa-2x android" style="color:#fff;"></i></a>
+    
     <span class="webarch">
     <img src="images/webarch.png" alt="Webarch" width="60%">
     </span>
