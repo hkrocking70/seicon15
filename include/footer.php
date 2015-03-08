f09756d317f77c1305baedd9233c3b2370b79d2e
Himanshu <himanshu.kumar133@gmail.com>
2015-03-08 15:54:43 +0530

    Footer update and minor changes

--- include/footer.php
+++ include/footer.php
@@ -1,13 +1,17 @@
-<div class="navbar navbar-fixed-bottom footer">
-  <div class="container" style="padding-top: 10px;">
-    <a href="https://www.facebook.com/pages/SEICON/894757557243320"><i class="fa fa-facebook fa-2x fb" style="color:#fff;"></i></a>
-    &nbsp;&nbsp;
-    <a href="#"><i class="fa fa-youtube-play fa-2x youtube" style="color:#fff;"></i></a>
-    &nbsp;&nbsp;
-    <a href="https://play.google.com/store/apps/details?id=com.appenstien.seicon"><i class="fa fa-android fa-2x android" style="color:#fff;"></i></a>
-    <span style='color:#fff; margin-left:102px;'>&copy; Copyright-WEBARCH-2015</span>
-    <span class="webarch">
-    <img src="images/webarch.png" alt="Webarch" width="60%">
-    </span>
+<div class= "navbar navbar-fixed-bottom  footer" >
+  <div class="col-md-4">
+        <a href="https://www.facebook.com/pages/SEICON/894757557243320"><i class="fa fa-facebook fa-2x fb" style="color:#fff;"></i></a>
+        &nbsp;&nbsp;
+        <a href="#"><i class="fa fa-youtube-play fa-2x youtube" style="color:#fff;"></i></a>
+        &nbsp;&nbsp;
+        <a href="https://play.google.com/store/apps/details?id=com.appenstien.seicon"><i class="fa fa-android fa-2x android" style="color:#fff;"></i></a>
+  </div>
+  <div class="col-md-4">
+    <span style="color:#FFF; font-size:12px; ">&copy; Copyrights Webarch 2015</span>
+  </div>
+  <div class="col-md-4">
+    <center>
+    <img src="images/webarch.png" alt="Webarch" width="40%"  class="img-responsive">
+    </center>
   </div>
 </div>
