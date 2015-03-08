61548a34da4f5e8c884d6e76c94ca2657f7a8e5d
Anshul Sahni <anshul_sahni@rediffmail.com>
2015-03-08 23:04:50 +0530

    date of abstract submission changed

--- include/news.php
+++ include/news.php
@@ -3,7 +3,7 @@
 </center>
 
 <ul style="font-size:15px;">
-    <li>The last date for abstract submission is 5th March 2015.</li>
-    <li>The dates of event are 19th and 20th March 2015.</li>
+    <li>The last date for abstract submission is 15<sup>th</sup> March 2015.</li>
+    <li>The dates of event are 19<sup>th</sup> and 20<sup>th</sup> March 2015.</li>
     <li>Submit Your abstracts. <A href="https://docs.google.com/forms/d/1GyJiOXMX3xOCLU_HoczWRBPajGkrc4FBANJPB9nYTzM/viewform?c=0&w=1" target="_blank">Click Here</a>.
 </ul>   
